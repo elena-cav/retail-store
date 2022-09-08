899ec63152043ce8b781ac2010467ba5aae78a06
Elena Cavallero <elecav@hotmail.it>
2022-09-08 15:40:15 +0200

    return product

--- src/product-service/handler.test.ts
+++ src/product-service/handler.test.ts
@@ -9,4 +9,13 @@ describe("ProductService", () => {
     const response = await handle(event);
     expect(response.statusCode).toBe(200);
   });
+  test("Should have got the correct product", async () => {
+    const event: APIGatewayProxyEvent = {
+      body: "",
+      pathParameters: { productSKU: "abc" },
+    } as any;
+    const response = await handle(event);
+    const product = JSON.parse(response.body) as Product;
+    expect(product.sku).toBe("abc");
+  });
 });
--- src/product-service/handler.ts
+++ src/product-service/handler.ts
@@ -1,7 +1,12 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-
+import product from "./product";
 export const handle = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  return { statusCode: 200, body: "Hello World" };
+  const products: product[] = [{ sku: "abc" }, { sku: "cvb" }];
+  const sku = event.pathParameters?.productSKU;
+
+  const foundProduct = products.find((product) => product.sku === sku);
+
+  return { statusCode: 200, body: JSON.stringify(foundProduct) };
 };
